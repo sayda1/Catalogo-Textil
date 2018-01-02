c71701ec80b7490ce45c6c16fa27d6dd2cf7efec
Saida Quintanilla <squintanilla@ongpazperu.interno>
2018-01-02 11:52:21 -0500

    enlaces listos

--- js/direciones.js
+++ js/direciones.js
@@ -9,6 +9,18 @@ function init(){
     $("#primerBuzo").click(PrimerBuzo);
     $("#segundoBuzo").click(segundoBuzo);
     $("#tercerBuzo").click(tercerBuzo);
+    //FUNCION BVD
+    $("#primerBvd").click(primerBvd);
+    $("#segundoBvd").click(segundoBvd);
+    $("#tercerBvd").click(tercerBvd);
+    //FUNCION  CAMISA
+    $("#primeraCamisa").click(primeraCamisa);
+    $("#segundaCamisa").click(segundaCamisa);
+    $("#terceraCamisa").click(terceraCamisa);
+    //FUNCCION CASACAS
+    $("#primeraCasaca").click(primeraCasaca);
+    $("#segundaCasaca").click(segundaCasaca);
+    $("#terceraCasaca").click(terceraCasaca);
 }
 
 //FUNCIONES DE BLUSAS
@@ -35,5 +47,40 @@ function segundoBuzo(){
 }
 //TERCERO
 function tercerBuzo(){
-    location.href=("buzo-tafeta.html")
+    location.href="buzo-tafeta.html"
 }
+//FUNCION DE BVDS
+//PRIMERO
+function primerBvd(){
+    location.href="bvd-algodon-30-1.html";
+}
+//SEGUNDO
+function segundoBvd(){
+    location.href="bvd-algodon-24-1.html"
+}
+//TERCERO
+function tercerBvd(){
+    location.href="bvd-algodon-20-1.html"
+}
+//FUNCION CAMISAS
+//PRIMERA 
+function primeraCamisa(){
+    location.href="camisa-manga-larga.html";
+}
+//SEGUNDA
+function segundaCamisa(){
+    location.href="camisa-manga-corta.html";
+}
+function terceraCamisa(){
+    location.href="camisa-aplicaciones.html"
+}
+//FUNCION CASACAS
+function primeraCasaca(){
+    location.href="casaca-hypora.html";
+}
+function segundaCasaca(){
+    location.href="casaca-taslan.html";
+}
+function terceraCasaca(){
+    location.href="casaca-taslan-tejido.html";
+}   
