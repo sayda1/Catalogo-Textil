1dc24afe702e3616d24fece0eeb74d0f8d4f1696
Saida Quintanilla <squintanilla@ongpazperu.interno>
2018-01-02 09:31:21 -0500

    responsive de sacos y enlases

---
+++ js/direciones.js
@@ -0,0 +1,25 @@
+$(document).ready(init);
+//FUNCION GENERAL
+function init(){
+    console.log(primerBoton);
+    $("#primero").click(primerBoton);
+    $("#segundo").click(segundoBoton);
+    $("#tercero").click(tercerBoton);
+}
+
+//FUNCIONES DE BOTONES
+//PRIMERO
+function primerBoton(){
+    location.href="blusa-manga-corta.html";
+}
+//SEGUNDO
+function segundoBoton(){
+    location.href="blusa-manga-larga.html";
+}
+function tercerBoton(){
+    location.href="blusa-manga-tres-cuartos.html";
+}
+//TERCERO
+//CUARTO
+//QUINTO
+//SEXTO
